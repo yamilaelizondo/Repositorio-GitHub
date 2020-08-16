4cb3c4d0e2583d2c708f652f20010af110aee113
yamilaelizondo <yamilaelizondo@users.noreply.github.com>
2020-08-16 19:49:16 -0300

    up

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function(e){
         formulario.loginButton.addEventListener("click", function(){ 
             if (formulario.userName.value=='yami@mail.com' && formulario.password.value=='1234'){ 
                 //document.form.submit(); 
-                window.location.href = "mainPage2.html";
+                window.location.href = "mainPage.html";
                 localStorage.setItem('usuario', formulario.userName.value);
                 localStorage.setItem('contraseña', formulario.password.value);
                 alert("El usuario conectado es " + localStorage.getItem('usuario'));
