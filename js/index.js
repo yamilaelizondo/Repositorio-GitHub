d9573d360e7fafeba3db52f6022548eb7b7c1a7b
yamilaelizondo <yamilaelizondo@users.noreply.github.com>
2020-08-15 23:49:13 -0300

    up

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+   
+        document.getElementById("loginButton").addEventListener("click", function(){ 
+            if (document.loginForm.userName.value=='yami@coso.com' && document.loginForm.password.value=='1234'){ 
+                //document.form.submit(); 
+                window.location.href = "mainPage.html";
+            } 
+            else{ 
+                 alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+            } 
+        });
+});
