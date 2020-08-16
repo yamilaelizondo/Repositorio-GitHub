5e2c424211852270a178b0605e824441f307933b
yamilaelizondo <yamilaelizondo@users.noreply.github.com>
2020-08-15 23:26:53 -0300

    up

--- js/products.js
+++ js/products.js
@@ -2,5 +2,48 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(CATEGORIES_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+       {
+           categoriesArray = resultObj.data;
+           //Muestro las categorías ordenadas
+           showCategoriesList(categoriesArray);
+       }
+   });
 
+  
 });
+
+var categoriesArray = [];
+
+function showCategoriesList(array){
+
+   let htmlContentToAppend = "";
+   for(let i = 0; i < array.length; i++){
+       let category = array[i];
+
+       htmlContentToAppend += `
+       
+       
+       
+       
+       <div class="comte">
+           <div class="row">
+               <div class="col-3">
+                   <img src="` + category.imgSrc + `" alt= "` + category.name+ `" class="img-thumbnail">
+               </div>
+               <div class="col">
+                   <div class="d-flex w-100 justify-content-between">   
+                       <h4 class="mb-1">`+ category.name+`</h4>
+                     
+                       <small class="text-muted">` + category.productCount + ` artículos</small>
+                   </div>
+                   <div>` + category.description+ `</div>
+               </div>
+           </div>
+       </div>
+       `
+
+       document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+   }
+}
