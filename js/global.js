541645bc1bef1753df8ca44fbd53da29e372c163
yamilaelizondo <yamilaelizondo@users.noreply.github.com>
2020-08-18 22:22:14 -0300

    up

--- js/global.js
+++ js/global.js
@@ -1,5 +1,5 @@
 const logueado = false;
-if (localStorage.getItem("usuario") == null || localStorage.getItem("usuario") == undefined || localStorage.getItem("usuario") == ""){
+if (localStorage.getItem("usuario") == undefined || localStorage.getItem("usuario") == null || localStorage.getItem("usuario") == ""){
     alert ("Debe iniciar sesión");
     window.location.href = "index.html"; 
 } else {
