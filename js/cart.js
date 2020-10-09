cf59cb7d5009d0befa3d478ff1c22f21b16e6670
yamilaelizondo <yamilaelizondo@users.noreply.github.com>
2020-10-09 02:30:40 -0300

    up

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,8 @@
 var cartInfo = [];
 var cartArticles = [];
-let shippingPercentage = 0.15;
+var shippingPercentage = 0.15;
+let DOLLAR_SYMBOL = "USD ";
+let PESO_SYMBOL = "UYU ";
 
 function costoTotal(){
     
@@ -8,7 +10,7 @@ function costoTotal(){
 
 document.addEventListener("DOMContentLoaded", function(e){
 
-    getJSONData(CART_INFO_URL).then(function(resultObj) {
+    getJSONData("https://japdevdep.github.io/ecommerce-api/cart/654.json").then(function(resultObj) {
         let htmlContentToAppend = "";
         if (resultObj.status === "ok") {
             cartInfo = resultObj.data;
@@ -16,11 +18,11 @@ document.addEventListener("DOMContentLoaded", function(e){
             for (var i = 0; i < cartInfo.articles.length; i++) {
                 htmlContentToAppend += `
                 <tr>
-                <th scope="row"><img src="`+ cartArticles[i].src +`" alt="" width="50px"></th>
-                <td>`+ cartArticles[i].name +`</td>
-                <td>`+ cartArticles[i].currency + ` `  + cartArticles[i].unitCost +`</td>
-                <td><input class="form-control" style="width: 60px;" type="number" id="contadorProd"></td>
-                <td class="font-weight-bold" id="productSubtotal">UYU `+ resultado +`</td>
+                    <th scope="row"><img src="`+ cartArticles[i].src +`" alt="" width="50px"></th>
+                    <td>`+ cartArticles[i].name +`</td>
+                    <td>`+ cartArticles[i].currency + ` `  + cartArticles[i].unitCost +`</td>
+                    <td><input class="form-control" style="width: 60px;" type="number" id="contadorProd">`+ cartArticles[i].count +`</td>
+                    <td class="font-weight-bold" id="productSubtotal">UYU 100</td>
                 </tr>`
               document.getElementById("tablaCarrito").innerHTML = htmlContentToAppend;
             }
@@ -30,8 +32,6 @@ document.addEventListener("DOMContentLoaded", function(e){
     document.getElementById("contadorProd").addEventListener("change",function(){
         var contadorArt = document.getElementById("contadorProd").value;
         var resultado = contadorArt * unitCost;
-       
-        
     });
 
     document.getElementById("premiumRadio").addEventListener("change", function(){
