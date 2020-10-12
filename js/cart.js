32b46e466f6494cec720ab6a7d8ab08fa1c8c698
yamilaelizondo <yamilaelizondo@users.noreply.github.com>
2020-10-11 22:55:10 -0300

    up

--- js/cart.js
+++ js/cart.js
@@ -1,52 +1,68 @@
 var cartInfo = [];
 var cartArticles = [];
-var shippingPercentage = 0.15;
-let DOLLAR_SYMBOL = "USD ";
-let PESO_SYMBOL = "UYU ";
+var subtotalParcial;
+var porcentajeEnvio = 0.15;
+var subTotalCompletoImporte = 0;  
+//let DOLLAR_SYMBOL = "USD ";
+//let PESO_SYMBOL = "UYU ";
 
-function costoTotal(){
-    
-}
 
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
 
-    getJSONData("https://japdevdep.github.io/ecommerce-api/cart/654.json").then(function(resultObj) {
+    getJSONData("https://japdevdep.github.io/ecommerce-api/cart/654.json").then(function (resultObj) {
         let htmlContentToAppend = "";
         if (resultObj.status === "ok") {
             cartInfo = resultObj.data;
             cartArticles = cartInfo.articles;
             for (var i = 0; i < cartInfo.articles.length; i++) {
+                if (cartArticles[i].currency == "USD"){
+                    cartArticles[i].unitCost = cartArticles[i].unitCost *40;
+                }
                 htmlContentToAppend += `
-                <tr>
-                    <th scope="row"><img src="`+ cartArticles[i].src +`" alt="" width="50px"></th>
-                    <td>`+ cartArticles[i].name +`</td>
-                    <td>`+ cartArticles[i].currency + ` `  + cartArticles[i].unitCost +`</td>
-                    <td><input class="form-control" style="width: 60px;" type="number" id="contadorProd">`+ cartArticles[i].count +`</td>
-                    <td class="font-weight-bold" id="productSubtotal">UYU 100</td>
+                <tr class= "articuloDeLista">
+                    <th scope="row"><img src="`+ cartArticles[i].src + `" alt="" width="50px"></th>
+                    <td>`+ cartArticles[i].name + `</td>
+                    <td class="currencyAndCost">` + cartArticles[i].unitCost + `</td>
+                    <td class="cantidadArt">
+                        <input class="form-control" style="width: 60px;" type="number" value=`+ cartArticles[i].count + `>
+                    </td>
+                    <td class="font-weight-bold productSubtotal"></td>
                 </tr>`
-              document.getElementById("tablaCarrito").innerHTML = htmlContentToAppend;
+                document.getElementById("tablaCarrito").innerHTML = htmlContentToAppend;
             }
-        } 
-    });
-    
-    document.getElementById("contadorProd").addEventListener("change",function(){
-        var contadorArt = document.getElementById("contadorProd").value;
-        var resultado = contadorArt * unitCost;
+            muestroSubTotal();
+            muestroTotalProductos();
+        }
     });
 
-    document.getElementById("premiumRadio").addEventListener("change", function(){
-        shippingPercentage = 0.15;
-        costoTotal();
-    });
-    
-    document.getElementById("expressRadio").addEventListener("change", function(){
-        shippingPercentage = 0.07;
-        costoTotal();
-    });
+    function muestroSubTotal(){
+        $('.articuloDeLista').each(function () {
+            var precioUnitario = $(this).children('.currencyAndCost').text();
+            var cantidad = $(this).children('.cantidadArt').children().val();
+            subtotalParcial = precioUnitario * cantidad;
+            $(this).children('.productSubtotal').text(subtotalParcial);
+        });
+    }
 
-    document.getElementById("standardRadio").addEventListener("change", function(){
-        shippingPercentage = 0.05;
-        costoTotal();
+    function muestroTotalProductos(){
+        subTotalCompletoImporte = 0;
+        $('.articuloDeLista').each(function () {
+            var subTotalCompleto = parseFloat($(this).children('.productSubtotal').text());
+            subTotalCompletoImporte += subTotalCompleto;
+            document.getElementById("productCostText").innerHTML = subTotalCompletoImporte;
+        });
+    }
+
+    document.body.addEventListener('input', function (evt) {
+        muestroSubTotal();
+        muestroTotalProductos();
     });
+   
+    $('.radio input').change(function () {
+        var subTotalFinal = $("input[name='optradio']:checked").text();
+        var claseEnvio = $("input[name='optradio']:checked").val();
+        var costoEnvio = subTotalFinal*claseEnvio;
+     });
 });
+
 
