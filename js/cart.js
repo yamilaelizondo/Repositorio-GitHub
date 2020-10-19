a0389fc119b9baaac36b51ad4fceba2a946c0329
yamilaelizondo <yamilaelizondo@users.noreply.github.com>
2020-10-18 23:54:54 -0300

    up

--- js/cart.js
+++ js/cart.js
@@ -3,8 +3,7 @@ var cartArticles = [];
 var subtotalParcial;
 var porcentajeEnvio = 0.15;
 var subTotalCompletoImporte = 0;
-//let DOLLAR_SYMBOL = "USD ";
-//let PESO_SYMBOL = "UYU ";
+var metodoDePago;
 
 
 document.addEventListener("DOMContentLoaded", function (e) {
@@ -15,8 +14,8 @@ document.addEventListener("DOMContentLoaded", function (e) {
             cartInfo = resultObj.data;
             cartArticles = cartInfo.articles;
             for (var i = 0; i < cartInfo.articles.length; i++) {
-                if (cartArticles[i].currency == "USD"){
-                    cartArticles[i].unitCost = cartArticles[i].unitCost *40;
+                if (cartArticles[i].currency == "USD") {
+                    cartArticles[i].unitCost = cartArticles[i].unitCost * 40;
                 }
                 htmlContentToAppend += `
                 <tr class= "articuloDeLista">
@@ -24,28 +23,33 @@ document.addEventListener("DOMContentLoaded", function (e) {
                     <td>`+ cartArticles[i].name + `</td>
                     <td class="currencyAndCost">` + cartArticles[i].unitCost + `</td>
                     <td class="cantidadArt">
-                        <input class="form-control" style="width: 60px;" type="number" value=`+ cartArticles[i].count + `>
+                        <input class="form-control" style="width: 60px;" type="number" min="0" value=`+ cartArticles[i].count + `>
                     </td>
                     <td class="font-weight-bold productSubtotal"></td>
+                    <td class="eliminarArt">
+                        <input class="btn btn-primary btn-sm eliminar" type="button" value="Eliminar">
+                    </td>
                 </tr>`
                 document.getElementById("tablaCarrito").innerHTML = htmlContentToAppend;
             }
             muestroSubTotal();
             muestroTotalProductos();
             actualizoTotales();
+            agregoListeners();
         }
     });
 
-    function muestroSubTotal(){
+    function muestroSubTotal() {
         $('.articuloDeLista').each(function () {
             var precioUnitario = $(this).children('.currencyAndCost').text();
             var cantidad = $(this).children('.cantidadArt').children().val();
             subtotalParcial = precioUnitario * cantidad;
-            $(this).children('.productSubtotal').text(subtotalParcial);
+            $(this).children('.productSubtotal').text(subtotalParcial + " $U");
         });
     }
 
-    function muestroTotalProductos(){
+
+    function muestroTotalProductos() {
         subTotalCompletoImporte = 0;
         $('.articuloDeLista').each(function () {
             var subTotalCompleto = parseFloat($(this).children('.productSubtotal').text());
@@ -54,20 +58,65 @@ document.addEventListener("DOMContentLoaded", function (e) {
         });
     }
 
-    document.body.addEventListener('input', function (evt) {
+    function agregoListeners() {
+        $('.radio input').change(function () {
+            actualizoTotales();
+        });
+        $('.eliminarArt input').click(function () {
+            eliminarArticulo(this);
+        });
+        $('.cantidadArt input').change(function () {
+            muestroSubTotal();
+            muestroTotalProductos();
+            actualizoTotales();
+        });
+        $('#metodoCredito').change(function () {
+           metodoDePago = $("input[name='pago']:checked").val();
+            $('#numeroDeTarjeta').prop( "disabled", false );
+            $('#añoExpiracion').prop( "disabled", false );
+            $('#mesExpiracion').prop( "disabled", false );
+            $('#codigoSeguridad').prop( "disabled", false );
+        });
+        $('#metodoBanco').change(function () {
+
+        });
+        $('#confirmButton').click(function () {
+            validoCampos();
+        });
+    }
+    function validoCampos() {
+        if (metodoDePago == "tCredito") {
+            var numeroDeTarjeta = "";
+            var anioDeExpiracion = "";
+            var mesDeExpiracion = "";
+            var codigoDeSeguridad = "";
+            numeroDeTarjeta = $('#numeroDeTarjeta').val();
+            anioDeExpiracion = $('#añoExpiracion').val();
+            mesDeExpiracion = $('#mesExpiracion').val();
+            codigoDeSeguridad = $('#codigoSeguridad').val();
+            if (numeroDeTarjeta == "" || anioDeExpiracion == "" || mesDeExpiracion == "" || codigoDeSeguridad == "") {
+                alert("Falta completar datos");
+            }
+        } else {
+
+        }
+    }
+
+    function eliminarArticulo(botonEliminar) {
+        $(botonEliminar).parent().parent().remove();
         muestroSubTotal();
         muestroTotalProductos();
         actualizoTotales();
-    });
-   
-    function actualizoTotales(){
-        $('.radio input').change(function () {
-            var costoDeEnvio= $("input[name='opcion']:checked").val() * $("#productCostText").text();
-            importeTotal = costoDeEnvio + parseFloat($("#productCostText").text());
-            document.getElementById("comissionText").innerHTML = costoDeEnvio.toFixed(0);
-            document.getElementById("totalCostText").innerHTML = importeTotal.toFixed(0);
-         });
     }
+
+
+    function actualizoTotales() {
+        var costoDeEnvio = $("input[name='opcion']:checked").val() * $("#productCostText").text();
+        importeTotal = costoDeEnvio + parseFloat($("#productCostText").text());
+        document.getElementById("comissionText").innerHTML = costoDeEnvio.toFixed(0);
+        document.getElementById("totalCostText").innerHTML = importeTotal.toFixed(0);
+    }
+
 
 });
 
